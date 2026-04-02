71002110a70676e9e7194291ff78e2d2eadf3e2b
Joris Wachsmuth <joris.wachsmuth@uni-bielefeld.de>
2026-04-02 14:01:40 +0200

    added script to plot raw osc and firmware data

---
+++ plot_raw_data.py
@@ -0,0 +1,24 @@
+import polars as pl
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Please provide path")
+        exit(-1)
+    path = sys.argv[1]
+
+    pico_data = pl.read_parquet(path + "/usb_osc_data.parquet")
+    firmware_data = pl.read_parquet(path + "/fast_firmware.parquet")
+
+    osc_idx = np.arange(start=0, stop=pico_data.shape[0]) * (1 / 5000000)
+    firm_idx = np.arange(start=0, stop=firmware_data.shape[0]) * (1 / 2000)
+
+    plt.plot(firm_idx, firmware_data["current"], label="firmware")
+    plt.plot(osc_idx, pico_data["current"], label="osc")
+
+    plt.legend()
+    plt.xlabel("seconds")
+    plt.ylabel("amp (not calibrated)")
+    plt.show()
